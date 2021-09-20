e2b0130665ff82fb4b8dba2b085f426200037383
nik idhan <you@example.com>
2021-09-21 00:49:06 +0800

    commit on 'nik'

--- task-assignment/src/firebase.js
+++ task-assignment/src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
+import 'firebase/firestore';
 
-const firebaseApp = firebase.initializeApp({
+const firebaseApp = firebase.initializeApp ({
     apiKey: "AIzaSyDOKn88vEsYjhvNerE-oadm8NmJ48pMTyM",
     authDomain: "task-assignment-aef22.firebaseapp.com",
     databaseURL: "https://task-assignment-aef22-default-rtdb.firebaseio.com",
     projectId: "task-assignment-aef22",
     storageBucket: "task-assignment-aef22.appspot.com",
     messagingSenderId: "922945396063",
-    appId: "1:922945396063:web:3b4b35e9a2c12684ea6aea",
-    measurementId: "G-H8DCPGGHF4"
+    appId: "1:922945396063:web:ade7ce818133dfd0ea6aea",
+    measurementId: "G-VVXTTL37TN"
 });
 
 const db = firebaseApp.firestore();
